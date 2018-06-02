7d7999661ceef3cac0c21b36d515e84629d9792c
aline bispo <aline.blima26@gmail.com>
2018-06-02 11:26:04 -0300

    javascript/leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -8,11 +8,8 @@ var Year = function (year) {
 };
 
 Year.prototype.isLeap = function () {
-  if ((this.year % 400 ===0) || (this.year % 4 ===0 && this.year % 100 !==0)){
-    return true;
-  } else{
-    return false;
-  }
+  return ((this.year % 400 ===0) || (this.year % 4 ===0 && this.year % 100 !==0))
+
 };
 
 module.exports = Year;
